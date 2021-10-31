1601a4858f3266e0f43ae1a428f7d58d5f11a66b
mrmoodswings <safwan.mst007@gmail.com>
2021-10-31 12:53:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+      email: "John@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const user = await fetch("https://randomuser.me/api");
+      const { results } = await user.json();
+      this.firstName = results[0].name.firstName;
+      this.lastName = results[0].name.lastName;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+app.mount("#app");
